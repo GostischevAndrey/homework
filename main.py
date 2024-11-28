5aa6421172ae7f41db45dafcf104cd01bdf2229a
Andrey <pise4kan8@gmail.com>
2024-11-29 00:10:12 +0300

    fixes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.widget import mask_account_card
+from src.masks import get_mask_card_number, get_mask_account
+from src.widget import mask_account_card, get_data
 
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
@@ -8,3 +9,7 @@ print(mask_account_card("Visa Classic 6831982476737658"))
 print(mask_account_card("Visa Platinum 8990922113665229"))
 print(mask_account_card("Visa Gold 5999414228426353"))
 print(mask_account_card("Счет 73654108430135874305"))
+print(get_mask_card_number("6831982476737658"))
+print(get_mask_account(35383033474447895560))
+print(get_mask_account("64686473678894779589"))
+print(get_data("2024-03-11T02:26:18.671407"))
