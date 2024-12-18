5df2aa68dcd6648035ca19487a236e88f80e0c13
Andrey <pise4kan8@gmail.com>
2024-12-18 18:18:53 +0300

    add functions in generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,23 +1,43 @@
-def filter_by_currency():
+from typing import Generator, Iterator, Union
+
+
+def filter_by_currency(my_list: list[dict], currency_code: str = "USD") -> Union[Iterator[dict], str]:
     """
     Функция, которая принимает на вход список словарей.
     Функция должна возвращать итератор, который поочередно выдает транзакции,
     где валюта операции соответствует заданной (например, USD)
     """
-    pass
+    if not my_list:
+        return "Список транзакций пуст"
+    if currency_code not in ["USD"]:
+        return "Код валюты не найден."
+
+    for transaction in my_list:
+        if transaction["operationAmount"]["currency"]["code"] == currency_code:
+            yield transaction
+    return iter([])
 
 
-def transaction_descriptions():
+def transaction_descriptions(my_list: list[dict]) -> Union[Iterator[dict], str]:
     """
     Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
     """
-    pass
+    result = (i.get("description") for i in my_list)
+    for x in result:
+        yield x
+    return iter([])
 
 
-def card_number_generator():
+def card_number_generator(start: int, stop: int) -> Generator[str]:
     """
     Функция для генерирования номера карта в диапазоне
     от 0000 0000 0000 0001
     до 9999 9999 9999 9999
     """
-    pass
+    for number in range(start, stop + 1):
+        card_number = str(number)
+        while len(card_number) < 16:
+            card_number = "0" + card_number
+        formated_card_number = f"{card_number[:4]} {card_number[4:8]} {card_number[8:12]} {card_number[12:]}"
+
+        yield formated_card_number
